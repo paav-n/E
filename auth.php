f1e001f48ce6c1cf260efaeebedb08de345a94bb
Paavan Nanda <paavannanda@gmail.com>
2019-03-13 23:56:04 -0400

    database setup

--- auth.php
+++ auth.php
@@ -6,17 +6,19 @@
  * Time: 9:53 PM
  */
 
-$hostname = 'db777190816.hosting-data.io';
-$username = "dbo777190816";
-$project  = "db777190816";
-$password = "b3_Yq8WXMwhLeTh!";
-$db = mysqli_connect($hostname,$username, $password ,$project);
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$host_name = 'db777190816.hosting-data.io';
+$database = 'db777190816';
+$user_name = 'dbo777190816';
+$password = 'b3_Yq8WXMwhLeTh!';
+
+$connect = mysqli_connect($host_name, $user_name, $password, $database);
+if (mysqli_errno()) {
+    die('<p>Failed to connect to MySQL: '.mysqli_error().'</p>');
+} else {
+    echo '<p>Connection to MySQL server successfully established.</p >';
 }
 
-mysqli_select_db( $db, $project );
+mysqli_select_db( $connect, $database );
 $username = $_POST [ "username" ] ;
 $password = $_POST [ "pass" ] ;
 $s="SELECT pass from users where username='username'";
