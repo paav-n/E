d70836502b252bf1043c50fbb34366a0e7636f26
jadsouza30 <jadsouza@usc.edu>
2019-07-12 13:31:22 -0400

    styled confirmations/rejection with everything

--- Table_Responsive/ContactFrom_v4/accept_handler.php
+++ Table_Responsive/ContactFrom_v4/accept_handler.php
@@ -70,12 +70,12 @@
 				<div class="container-contact100-form-btn">
 					<div class="wrap-contact100-form-btn">
 						<div class="contact100-form-bgbtn"></div>
-						<input type="submit" class="contact100-form-btn">
+						<button class="contact100-form-btn">
 							<span>
 								Submit
 								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
 							</span>
-						</input>
+						</button>
 					</div>
 				</div>
 			</form>
