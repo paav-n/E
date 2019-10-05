771ec8cd1acae51e3762afbc5bea60d0fea70e0c
paav-n <paavannanda@gmail.com>
2019-10-05 04:53:45 -0400

    checkout button

--- menu/index.php
+++ menu/index.php
@@ -116,7 +116,7 @@ $w=mysqli_query($connect,$results);
                             <p class="pr-70"><?php echo $row['ITEM_DESCRIPTION'];?></p>
                         </div><!--s-right-->
                     </div><!-- sided-90x -->
-                    <button onclick="push<?php echo $row[ITEM_ID];?>()">Add To Cart</button>
+                    <button class="checkout" onclick="push<?php echo $row[ITEM_ID];?>()">Add To Cart</button>
                     <script type="text/javascript">
                         function push<?php echo $row[ITEM_ID];?>(){
                             items.push(<?php echo $row[ITEM_ID];?>);
@@ -136,39 +136,10 @@ $w=mysqli_query($connect,$results);
                 </div><!-- sided-90x -->
             </div>
         </div><!-- row -->
-
-        <h6 class="center-text mt-40 mt-sm-20 mb-30"><a href="#" class="btn-primaryc plr-25"><b>SEE TODAYS MENU</b></a></h6>
     </div><!-- container -->
 </section>
 
 <footer class="pb-50  pt-70 pos-relative">
-    <div class="pos-top triangle-bottom"></div>
-    <div class="container-fluid">
-        <a href="index.html"><img src="images/logo-white.png" alt="Logo"></a>
-
-        <div class="pt-30">
-            <p class="underline-secondary"><b>Address:</b></p>
-            <p>481 Creekside Lane Avila Beach, CA 93424 </p>
-        </div>
-
-        <div class="pt-30">
-            <p class="underline-secondary mb-10"><b>Phone:</b></p>
-            <a href="tel:+53 345 7953 32453 ">+53 345 7953 32453 </a>
-        </div>
-
-        <div class="pt-30">
-            <p class="underline-secondary mb-10"><b>Email:</b></p>
-            <a href="mailto:yourmail@gmail.com"> yourmail@gmail.com</a>
-        </div>
-
-        <ul class="icon mt-30">
-            <li><a href="#"><i class="ion-social-pinterest"></i></a></li>
-            <li><a href="#"><i class="ion-social-facebook"></i></a></li>
-            <li><a href="#"><i class="ion-social-twitter"></i></a></li>
-            <li><a href="#"><i class="ion-social-dribbble-outline"></i></a></li>
-            <li><a href="#"><i class="ion-social-linkedin"></i></a></li>
-            <li><a href="#"><i class="ion-social-vimeo"></i></a></li>
-        </ul>
 
         <p class="color-light font-9 mt-50 mt-sm-30"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ion-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
